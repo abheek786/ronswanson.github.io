ec0ac149518f766f1bf3f5e9c18cdcd4b430978c
abheek786 <rabheek@gmail.com>
2019-07-14 03:02:01 -0400

    Ron Swanson Fun App

---
+++ app.js
@@ -0,0 +1,11 @@
+function sad(){
+  document.getElementById("ron").src= "https://images.alphacoders.com/549/thumb-1920-549122.jpg";
+}
+
+function happy(){
+  document.getElementById("ron").src= "https://i.redd.it/cgwt0v6lgon11.jpg";
+}
+
+function angry(){
+  document.getElementById("ron").src= "https://i.imgur.com/BsENp.jpg";
+}
